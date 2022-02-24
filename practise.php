9003e2fd595d979867d8fe3737b1f10d7b70ee01
Abdul Khalaque <limonbabu165@gmail.com>
2022-02-25 00:48:43 +0600

    add

--- practise.php
+++ practise.php
@@ -1,18 +1,27 @@
 <?php
 
 class person{
-    public $name = "Limon";
-    public $age = 24;
+    public $name = "My age";
+    public $age;
     public $lim0n;
-    public function myfuction(){
-         $this->age;
+    public function func()
+    {
+     return  $this->age;
     }
+    public function myfuction($myage){
+         $this->age = $myage;
+    }
+}
+class childs extends person{
+    
 }
 $obj1 = new person;
 
 echo $obj1->lim0n = "Limon" . "<br>";
-echo $obj1->myfuction();
-
+ $obj1->myfuction(24);
+ echo $obj1->func() ."<br>";
+ 
+echo (new childs)->name;
  
 
 ?>
